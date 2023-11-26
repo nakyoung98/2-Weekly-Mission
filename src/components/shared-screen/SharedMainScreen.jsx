2b756e2477550194641dd83d6a00b2a2148697f1
nakyoung98 <66313756+nakyoung98@users.noreply.github.com>
2023-11-27 00:10:55 +0900

    [MOD] SharedMainScreen 데이터 fetch 처리
    - useEffect를 사용햔 folder 페이지 데이터 처리

--- src/components/shared-screen/SharedMainScreen.jsx
+++ src/components/shared-screen/SharedMainScreen.jsx
@@ -1,11 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import Card from '../commons/Card';
+import React, { useEffect, useState } from "react";
+import ReactDOM from "react-dom/client";
 
-const root = ReactDOM.createRoot(document.getElementById('main-root'));
+import ENDPOINTS from "../../config/api";
+
+import CardFactory from "../commons/CardFactory";
+import FolderInfo from "./FolderInfo";
+import SearchBar from "../commons/SearchBar";
+import TableLayout from "../layouts/TableLayout";
+
+const root = ReactDOM.createRoot(document.getElementById("main-root"));
 root.render(<SharedMainScreen />);
-
-const classNames = [`card`];
 
 function SharedMainScreen() {
     return (
@@ -13,4 +17,26 @@ function SharedMainScreen() {
         <Card classNames={classNames}/>
         </div>
     );
-}
+  const [folderData, setFolderData] = useState([]);
+  const [linkData, setLinkData] = useState([]);
+
+  useEffect(() => {
+    fetch(ENDPOINTS.SAMPLE.FOLDER)
+      .then((response) => response.json())
+      .then((result) => {
+        return result.folder;
+      })
+      .then((folder) => {
+        setFolderData({
+          id: folder.id,
+          name: folder.name,
+          owner: folder.owner,
+        });
+        return folder.links;
+      })
+      .then((links) => {
+        setLinkData(links);
+      });
+  }, []);
+
+  return (
