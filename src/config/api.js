fabad09d32b753d71ce917c34cb9ec5a3db7238a
nakyoung98 <66313756+nakyoung98@users.noreply.github.com>
2023-11-27 00:08:54 +0900

    [ADD] api url 저장 모듈 생성
    - api.js

---
+++ src/config/api.js
@@ -0,0 +1,10 @@
+const API_BASE_URL = "https://bootcamp-api.codeit.kr/api";
+
+const ENDPOINTS = {
+    SAMPLE: {
+        FOLDER: `${API_BASE_URL}/sample/folder`,
+        USER: `${API_BASE_URL}/sample/user`,
+    }
+};
+
+export default ENDPOINTS;
