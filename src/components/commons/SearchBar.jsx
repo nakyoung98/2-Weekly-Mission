d0d7c05489ac3488c87a9121ccbb4b90ffa95f2a
nakyoung98 <66313756+nakyoung98@users.noreply.github.com>
2023-11-27 00:18:32 +0900

    [ADD] SearchBar 컴포넌트 생성
    - SearchBar.jsx

---
+++ src/components/commons/SearchBar.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function SearchBar() {
+  return (
+    <div>
+      {/* Your JSX code for the search bar component */}
+    </div>
+  );
+}
+
+export default SearchBar;
