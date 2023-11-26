3fba25cfd9f950eb11221dd3aaa639e76c074cfa
nakyoung98 <66313756+nakyoung98@users.noreply.github.com>
2023-11-27 02:05:56 +0900

    [MOD] SearchBar 컴포넌트 구현
    데이터 검색 로직  제외 디자인 구현

--- src/components/commons/SearchBar.jsx
+++ src/components/commons/SearchBar.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 
+import SearchIcon from '../../../image/search/Search.svg'
+
+import './SearchBar.css';
+
 function SearchBar() {
   return (
-    <div>
-      {/* Your JSX code for the search bar component */}
-    </div>
+    <form className='search-bar'>
+      <img className='search-bar__search-icon' src={SearchIcon} alt='검색 아이콘'/>
+      <input className='search-bar__input' type="text" placeholder={`링크를 검색해 보세요.`} />
+    </form>
   );
 }
 
